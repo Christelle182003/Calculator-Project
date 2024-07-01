d51b2d612708a432b56bfed12a235c514c07d8c3
Christelle18 <vigninouavodaho@gmail.com>
2024-07-01 16:56:36 +0100

    commiiiiiit

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    let currentTheme = 'style1.css'; //  default Theme
+    const themeStyles = {
+        'style1': 'styles.css',
+        'style2': 'styles2.css',
+        'style3': 'styles3.css'
+    };
+  
+    themeToggle.addEventListener('click', function() {
+        // Change theme
+        switchTheme();
+    });
+  
+    function switchTheme() {
+        // next theme
+        if (currentTheme === 'style1') {
+            currentTheme = 'style2';
+        } else if (currentTheme === 'style2') {
+            currentTheme = 'style3';
+        } else {
+            currentTheme = 'style1';
+        }
+  
+        // Change CSS
+        loadThemeStyles(currentTheme);
+    }
+  
+    function loadThemeStyles(theme) {
+        const styleLink = document.getElementById('theme-style');
+        styleLink.href = themeStyles[theme];
+    }
+  });
+  
+
+
+
 const touches = [...document.querySelectorAll('.number')];
     //console.log(touches);
 const listKeyCodes = touches.map(touche => touche.dataset.key)
